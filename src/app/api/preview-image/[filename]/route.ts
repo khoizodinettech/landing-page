1a4d5ac1bba43cb531456960f8053447b40747ac
khoizodinettech <khoi.vu@zodinet.tech>
2025-08-20 11:23:27 +0700

    fixbugdeploy invalid "Get" export

--- src/app/api/preview-image/[filename]/route.ts
+++ src/app/api/preview-image/[filename]/route.ts
@@ -3,9 +3,9 @@ import { ImageService } from "../../../../lib/service/image.service";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { filename: string } }
+  { params }: { params: Promise<{ filename: string }> }
 ) {
-  const { filename } = params;
+  const { filename } = await params;
   try {
     const image = await ImageService.getInstance().getImage(filename);
     return new NextResponse(image, {
